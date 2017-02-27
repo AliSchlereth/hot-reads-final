1708918abf47311c8930ff33baf9f161a602f26e
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-27 15:27:52 -0700

    Update api/v1/links to increase count appropriately

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::LinksController < ApplicationController
 
   def create
     link = Link.find_or_initialize_by(url: params[:link])
+    link.count += 1
     if link.save
-      link.count += 1
       hot_reads = Link.top_ten
       render json: {message: "Updated Hot Reads", current_hot_reads: hot_reads}
     else
